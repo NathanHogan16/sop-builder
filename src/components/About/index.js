2ae2c9bd2838de0e8faed2290df0766c0197338f
Noot <natek.hogan@gmail.com>
2023-03-12 15:56:59 -0600

    added first code to main page

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = ()=>{
+    return (
+        <section>
+            <h1 id="about">Welcome!</h1>
+        </section>
+    );
+}
+
+
+export default About;
